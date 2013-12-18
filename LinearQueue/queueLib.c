20e71ccf151ef59dfe03ba33c3573cb9d4dac148
unknown <sandeshm@INsandeshm.corporate.thoughtworks.com>
2013-12-18 12:18:32 +0530

    Linear queue modified

--- LinearQueue/queueLib.c
+++ LinearQueue/queueLib.c
@@ -1,9 +1,9 @@
 #include "queueLib.h"
 #include <stdlib.h>
 #include <string.h>
+#include <stdio.h>
 
-Queue* create(int typeSize, int noOfElements)
-{
+Queue* create(int typeSize, int noOfElements){
 	Queue *queue = malloc(sizeof(Queue));
 	queue->elements = calloc(noOfElements,typeSize);
 	queue->noOfElements = noOfElements;
@@ -14,16 +14,25 @@ Queue* create(int typeSize, int noOfElements)
 }
 
 int enqueue(Queue *queue, void *inputElement){
-	if(queue->rear == queue->noOfElements-1) return 0;
+	if(queue->rear == queue->noOfElements-1) 
+		return 0;
+
 	queue->rear = queue->rear + 1;
 	memcpy(queue->elements+(queue->rear*queue->elementSize),inputElement,queue->elementSize);
 	return 1;
 }
 
+
 void* dequeue(Queue *queue){
 	void* removedElement;
+	void* firstElement;
+	void* successiveElements;
 	if(queue->rear == -1) return NULL;
-	removedElement = queue->elements + (queue->front * queue->elementSize);
-	queue->front = queue->front + 1;	
-		return removedElement; 
+
+	firstElement = queue->elements;
+	successiveElements = queue->elements + queue->elementSize;
+	memcpy(firstElement,successiveElements,(queue->noOfElements-1)*queue->elementSize);
+	queue->rear = queue->rear - 1;	
+
+	return queue->elements;
 }
--- LinearQueue/queueLibTest.c
+++ LinearQueue/queueLibTest.c
@@ -3,9 +3,9 @@
 #include <stdlib.h>
 #include "testUtils.h"
 #include "queueLib.h"
+
 //create setup, tearDown, fixtureSetup, fixtureTearDown methods if needed
 
-// Queue expectedQueue;
 
 int areEqual(Queue a, Queue b)
 {
@@ -42,6 +42,7 @@ void test_create_creates_queue_of_floatType(){
 	ASSERT(areEqual(expectedQueue,*queuePtr));
 	free(queuePtr);
 }
+//-------------------------------------------------------
 
 void test_Enqueue_element_into_queue_through_rearEnd(){
 	int inputElement = 9;
@@ -51,46 +52,45 @@ void test_Enqueue_element_into_queue_through_rearEnd(){
 	Queue expectedQueue = {intArray,sizeof(int),0,0,4};
 	queuePtr = create(sizeof(int),4);
 	result = enqueue(queuePtr,&inputElement);
+	// printf("%d\n",*(int*)queuePtr->elements );
 	ASSERT(areEqual(expectedQueue,*queuePtr));
 }
 
-void test_Enqueue_FloatElement_into_queue_through_rearEnd(){
-	float inputElement = 9.223f;
-	float input2 = 7.989f;
-	Queue *queuePtr;
-	int result;
-	float floatArray[4] = {9.223f,7.989f,0.0,0.0};
-	Queue expectedQueue = {floatArray,sizeof(float),0,1,4};
-	queuePtr = create(sizeof(float),4);
-	enqueue(queuePtr,&inputElement);
-	result = enqueue(queuePtr,&input2);
-	ASSERT(areEqual(expectedQueue,*queuePtr));
-}
 
-void test_Enqueue_element_into_charQueue_through_rearEnd(){
-	char inputElement = 'd';
+void test_Enqueue_element_into_queue_through_rearEnd_for_charType(){
+	char inputElement = 'c';
 	Queue *queuePtr;
 	int result;
-	char charArray[4] = {'d',0,0,0};
+	char charArray[4] = {'c',0,0,0};
 	Queue expectedQueue = {charArray,sizeof(char),0,0,4};
 	queuePtr = create(sizeof(char),4);
 	result = enqueue(queuePtr,&inputElement);
 	ASSERT(areEqual(expectedQueue,*queuePtr));
 }
 
-void test_Enqueue_element_should_return_0_if_queue_is_full(){
-	int inputElement = 9;
+
+void test_Enqueue_element_into_queue_through_rearEnd_floatType(){
+	float inputElement = 9.23f;
 	Queue *queuePtr;
 	int result;
-	// Queue expectedQueue = {intArray,sizeof(int),0,0,4};
-	queuePtr = create(sizeof(int),4);
-	enqueue(queuePtr,&inputElement);
-	enqueue(queuePtr,&inputElement);
-	enqueue(queuePtr,&inputElement);
-	enqueue(queuePtr,&inputElement);
+	float floatArray[4] = {9.23f,0,0,0};
+	Queue expectedQueue = {floatArray,sizeof(float),0,0,4};
+	queuePtr = create(sizeof(float),4);
 	result = enqueue(queuePtr,&inputElement);
-	ASSERT(0==result);
+	ASSERT(areEqual(expectedQueue,*queuePtr));
 }
+void test_Enqueue_should_return_0_when_queue_is_full(){
+	int inputElement = 33;
+	int input = 17;
+	int value;
+	Queue *queuePtr;
+	queuePtr = create(sizeof(int),2);
+	enqueue(queuePtr,&inputElement);
+	enqueue(queuePtr,&input);
+	value = enqueue(queuePtr,&input);
+	ASSERT(0==value);
+}
+
 //---------------------------Dequeue-------------------------------
 
 void test_Dequeue_should_return_NULL_when_Queue_is_empty(){
@@ -100,41 +100,71 @@ void test_Dequeue_should_return_NULL_when_Queue_is_empty(){
 	queuePtr = create(sizeof(int),4);
 	result = dequeue(queuePtr);
 	ASSERT(NULL == result);
+	free(queuePtr);
 }
 
 void test_Dequeue_should_remove_element_from_Queue(){
-	int inputElement = 9;
+	int inputElement = 33;
 	int input = 17;
 	Queue *queuePtr;
 	int *value;
 	queuePtr = create(sizeof(int),4);
 	enqueue(queuePtr,&inputElement);
 	enqueue(queuePtr,&input);
+	
 	value = dequeue(queuePtr);
-	ASSERT(9==*value);
+	ASSERT(17==*value);
 }
 
 void test_Dequeue_should_remove_element_from_floatQueue(){
-	float inputElement = 9.34;
-	float input2 = 88.23;
+	float input = 33.23f;
+	float input2 = 564.112f;
+	float input3 = 88.074f;
 	Queue *queuePtr;
 	float *value;
 	queuePtr = create(sizeof(float),4);
-	enqueue(queuePtr,&inputElement);
+	enqueue(queuePtr,&input);
 	enqueue(queuePtr,&input2);
-	dequeue(queuePtr);
+	enqueue(queuePtr,&input3);
+	
 	value = dequeue(queuePtr);
-	ASSERT(88.23f==*value);
+	ASSERT(564.112f == *value);
 }
 
 void test_Dequeue_should_remove_element_from_charQueue(){
-	char inputElement = 'q';
-	char input = 'j';
+	char inputElement = 'a';
+	char input = 'b';
 	Queue *queuePtr;
 	char *value;
 	queuePtr = create(sizeof(char),4);
 	enqueue(queuePtr,&inputElement);
 	enqueue(queuePtr,&input);
+	
 	value = dequeue(queuePtr);
-	ASSERT('q'==*value);
+	// printf("%c\n",*value );
+	ASSERT('b'==*value);
+}
+
+void test_Dequeue_should_remove_element__and_reposition_Queue(){
+	int input = 33;
+	int input2 = 17;
+	int input3 = 99;
+	int input4 = 123;
+	int input5 = 789;
+	Queue *queuePtr;
+	int *value;
+	queuePtr = create(sizeof(int),5);
+	enqueue(queuePtr,&input);
+	enqueue(queuePtr,&input2);
+	enqueue(queuePtr,&input3);
+	enqueue(queuePtr,&input4);
+	enqueue(queuePtr,&input5);
+
+	value = dequeue(queuePtr);
+	// printf("%d\n",*value );
+	value = dequeue(queuePtr);
+	// printf("%d\n",*value );
+	value = dequeue(queuePtr);
+	// printf("rear====>%d\n\n======>%d\n",queuePtr->rear,*value );
+	ASSERT(123==*value);
 }
