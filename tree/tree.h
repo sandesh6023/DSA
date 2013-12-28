6fb14075bedb8029ed2df5aa93e6dd40509d35f1
unknown <sandeshm@INsandeshm.corporate.thoughtworks.com>
2013-12-28 11:48:58 +0530

    tree deleted

--- tree/tree.h
+++
@@ -1,9 +0,0 @@
-#include "doubleLL.h"
-typedef int (*compare)(void* ID, void* data);
-typedef struct{
-	void* root;
-	compare cmp;
-} Tree;
-
-Tree createTree(compare comp);
-int insertNode(Tree* tree,void* parentData,void* data);
